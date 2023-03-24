0dfa771a2e6366690e8be276945d7f1afe673c0f
Utkarsh Raj <utraj00114@gmail.com>
2023-03-24 14:48:18 +0530

    java downgrade

--- src/main/java/tech/theroyalraj/gpt/OpenAIClientApplication.java
+++ src/main/java/tech/theroyalraj/gpt/OpenAIClientApplication.java
@@ -2,11 +2,12 @@ package tech.theroyalraj.gpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class OpenAIClientApplication {
+public class OpenAIClientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OpenAIClientApplication.class, args);
